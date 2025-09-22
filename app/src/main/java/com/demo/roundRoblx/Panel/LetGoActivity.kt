02eccc8d447811d4af1bb72a82b5e41a02706489
Harsh Lashkarbhai Dhameliya <86516970+harshDhameliya6592@users.noreply.github.com>
2025-09-22 21:57:24 +0530

    1. LetGoActivity.kt and StartActivity.kt and WorkSpaceActivity.kt Done.

--- app/src/main/java/com/demo/roundRoblx/Panel/LetGoActivity.kt
+++ app/src/main/java/com/demo/roundRoblx/Panel/LetGoActivity.kt
@@ -1,18 +1,24 @@
 package com.demo.roundRoblx.Panel
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 import com.demo.roundRoblx.R
 import com.demo.roundRoblx.assignmentData.RoundStructureData
 import com.demo.roundRoblx.databinding.ActivityLetGoBinding
 import com.demo.roundRoblx.various.BeginApplication
 import com.demo.roundRoblx.various.Unique
+import kotlin.jvm.java
 
 class LetGoActivity : AppCompatActivity() {
 
@@ -56,10 +62,77 @@ class LetGoActivity : AppCompatActivity() {
             }
         }
 
-
+        if (first.round_small != null) {
+            binding.biloImage.visibility = View.VISIBLE
+            Glide.with(binding.biloImage)
+                .load(first.round_small.random().round_main_image)
+                .into(binding.biloImage)
+        } else {
+            binding.biloImage.visibility = View.GONE
+        }
     }
 
     private fun setPushEvent(first: RoundStructureData?) {
+        binding.layoutLetStart.setOnClickListener {
+            if (first != null && first.round_small_2 != null && !Unique.isRoundEmptyString(first.round_status) && first.round_status == "1") {
+                startActivity(Intent(this@LetGoActivity, StartActivity::class.java))
+                BeginApplication.showRoundRowTab(
+                    this@LetGoActivity,
+                    first.round_small_2.random().round_main_image?.toUri()
+                )
+            } else {
+                startActivity(Intent(this@LetGoActivity, StartActivity::class.java))
+            }
+        }
+
+        binding.layoutRateApp.setOnClickListener {
+            try {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=$packageName")
+                    )
+                )
+            } catch (e: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https://play.google.com/store/apps/details?id=$packageName")
+                    )
+                )
+            }
+        }
+
+        binding.layoutShareApp.setOnClickListener {
+            val roundIntent = Intent()
+            roundIntent.action = "android.intent.action.SEND"
+            roundIntent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Check out the App at: https://play.google.com/store/apps/details?id=$packageName"
+            )
+            roundIntent.type = "text/plain"
+            startActivity(roundIntent)
+        }
+
+        binding.layoutPrivacyPolicyApp.setOnClickListener {
+            if (first != null && !Unique.isRoundEmptyString(first.round_pcs)) {
+                try {
+                    val intent =
+                        Intent(Intent.ACTION_VIEW, Uri.parse(first.round_pcs))
+                    startActivity(intent)
+                } catch (e: Exception) {
+
+                }
+            } else {
+                Toast.makeText(
+                    this@LetGoActivity,
+                    getString(R.string.please_on_internet),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+
 
     }
 }
