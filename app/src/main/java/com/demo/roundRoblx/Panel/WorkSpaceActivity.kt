402a0528aa156986c399555a803e377eab50f447
Harsh Lashkarbhai Dhameliya <86516970+harshDhameliya6592@users.noreply.github.com>
2025-10-03 21:03:28 +0530

    1. Action Bar Done in WorkSpaceActivity.kt.

--- app/src/main/java/com/demo/roundRoblx/Panel/WorkSpaceActivity.kt
+++ app/src/main/java/com/demo/roundRoblx/Panel/WorkSpaceActivity.kt
@@ -4,18 +4,33 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.demo.roundRoblx.R
+import com.demo.roundRoblx.databinding.ActivityWorkSpaceBinding
 
 class WorkSpaceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWorkSpaceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_work_space)
+        binding = ActivityWorkSpaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        // Show the system bars.
+        windowInsetsController.show(WindowInsetsCompat.Type.systemBars())
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars =
+            true
+
+
+
     }
 }
